d199b34ffe28e4cfd6a262a8be55785c850a2a8e
anassant0s <ana.nascimento.santos26@escola.pr.gov.br>
2025-05-20 14:35:59 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'geografia',
+    'qual é o maior deserto do mundo em área?',
+    'Antártica'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais são cinso países que fazem fronteira com o Brasil?',
+    'Argentina,Bolívia, colômbia, Peru e venezuela'
+)
+
+criaCartao(
+    'geografia',
+    'o que é linha do equador e quais continentes ela atravessa?',
+    'linha imaginária que divide os hemisférios, atravessa América do sul, Africa e Ásia'
+)
+
+criaCartao(
+    'geografia',
+    'qual é o rio mais extenso do mundo?',
+    'Rio nilo'
+)
+
+criaCartao(
+    'geografia',
+    'Qual é o país mais populoso do mundo atualmente?',
+    'Índia'
+)
+
+criaCartao(
+    'geografia',
+    "o que são placas tectônicas e como elas influenciam os terremotos?,",
+    'Blocos de crostas que causam terremotos ao se moverem'
+)
